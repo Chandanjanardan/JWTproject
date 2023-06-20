1b663f24339119daa3f768b8ea7abf6406ab91e3
Chandan Janardan <chandanjanardan@gmail.com>
2023-06-20 07:43:28 +0530

    adding jwt verify

--- controllers/main.js
+++ controllers/main.js
@@ -3,7 +3,9 @@
 // send back to front=end
 
 // setup authentication so only the request with jwt can access the dashboard
-const customAPIError = require("../errors/custom-error")
+const CustomAPIError = require("../errors/custom-error")
+const jwt = require("jsonwebtoken")
+
 
 const test = async(req,res)=>{
     res.json({msg:"Hello this is test rotes and controller"})
@@ -12,17 +14,37 @@ const test = async(req,res)=>{
 const login = async(req,res)=>{
     const {username,password}=req.body
     if(!username || !password){
-
+        throw new CustomAPIError("Please provide email and password",400)
     }
+    // demo 
+    const id =new Date().getDate()
 
-    // 
-
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{
+        expiresIn:"30d"
+    })
+    res.status(200).json({msg:`User created`, token})
     console.log(username,password)
-    res.send("Fake login/register route")
+    // res.send("Fake login/register route")
 }
 const dashboard = async (req,res)=>{
-
+    console.log(req.headers)
+    const authHeader = req.headers.authorization;
+    if(!authHeader ){
+        throw new CustomAPIError("No token",401)
+    }
+    const token = authHeader.split(" ")[1]
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        // I set this line after decoded 
         const luckyNumber = Math.floor(Math.random()*1000)
+        res.status(200).json({msg:`Hello , john Doe `,
+    secret:`${luckyNumber}`})
+        console.log(decoded)
+    } catch (error) {
+        throw new CustomAPIError("Not authorized to access this route",401)
+    }
+    console.log(token)
+    const luckyNumber = Math.floor(Math.random()*1000)
         res.status(200).json({msg:`Hello , john Doe `,
     secret:`${luckyNumber}`})
         
